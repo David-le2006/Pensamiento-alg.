4b38ad4c76464774248392e6498a83195e71fa1f
David-le2006 <davinchirri@gmail.com>
2025-02-17 07:26:34 -0500

    Create tercer_punto.cpp

---
+++ tercer_punto.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    double Valor = 10;
+
+    //aplicar operaciones
+    Valor += 5;
+    Valor -= 3;
+    Valor *= 2;
+    Valor /= 4;
+
+    cout << "El valor final es de " << Valor << endl;
+
+    return 0;
+}
